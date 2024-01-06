bdebd2fafddd18e10bbb58623ef72702f1b6a999
Dhiraj <jireldhiraj123@gmail.com>
2024-01-06 14:59:18 +0545

    create home, admin and other component

---
+++ angular-app/src/app/admin/personal-detail-form/personal-detail-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-detail-form',
+  templateUrl: './personal-detail-form.component.html',
+  styleUrl: './personal-detail-form.component.scss'
+})
+export class PersonalDetailFormComponent {
+
+}
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { HeaderComponent } from './admin/header/header.component';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
+import { FooterComponent } from './admin/footer/footer.component';
+import { PersonalDetailFormComponent } from './admin/personal-detail-form/personal-detail-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LayoutComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    PersonalDetailFormComponent
   ],
   imports: [
     BrowserModule,
